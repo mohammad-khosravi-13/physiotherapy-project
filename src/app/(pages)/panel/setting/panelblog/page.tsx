4cc915bf2bb1f71c43b6566fc61dd312329e1e8b
MOHAMMAD <mohamad.bs.1382@gmail.com>
2024-11-09 14:05:28 +0330

    Added panelblog page

---
+++ src/app/(pages)/panel/setting/panelblog/page.tsx
@@ -0,0 +1,24 @@
+import Tableblogpanel from '@/app/container/setting/panelblog/Tableblogpanel';
+import Crad from '@/components/dashbord/Crad';
+import Navbar from '@/components/dashbord/Navbar'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div className=" w-[1248px] flex gap-9 h-[972px] m-auto ">
+        <div>
+            <Crad/>
+        </div>
+        <div className=' mt-12 w-[80%]'>
+         <Tableblogpanel/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page

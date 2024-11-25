9e92effc964cf9eff387e55250adb296f1edaa1d
MOHAMMAD <mohamad.bs.1382@gmail.com>
2024-11-25 21:59:32 +0330

    Describe the changes here

--- src/app/container/dashborde/Chertpanel.jsx
+++ src/app/container/dashborde/Chertpanel.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Productsuperior from "@/app/container/dashborde/Productsuperior";
+import Productsuperior from "@/container/dashborde/Productsuperior";
 import {
   LineChart,
   Line,
